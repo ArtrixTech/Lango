7b72f9164e5e99ce66caa7b38df10932b76c5c43
Artrix <Artrix@artrix.tech>
2018-10-25 18:56:50 +0800

    [JS] Edited location obtaning

--- lango.js
+++ lango.js
@@ -1,10 +1,25 @@
 function Lango() { }
 
-
-Lango.prototype.Language = {
-    lang_zh_cn: "ZH_CN",
-    lang_en_us: "EN_US"
+Lango.prototype.countryCode = undefined;
+Lango.prototype.Languages = {
+    lang_zh_cn: "ZH_CN",    // Chinese
+    lang_en_us: "EN_US",    // English - United States
+    lang_ja_jp: "JA_JP",     // Japanese - Japan
+    lang_ko_kr: "KO_KR",     // Korean - Korea
+    lang_es_us: "ES_US",     // Spanish - United States
+    lang_es_es: "ES_ES"      // Spanish - Spanish
 };
+
+Lango.prototype.isLangExist = function (lang) {
+
+    for (key in this.Languages) {
+        console.log("L:" + this.Languages[key] + " In:" + lang)
+        if (this.Languages[key] == lang) return true;
+    }
+    return false;
+
+}
+
 
 Lango.prototype.init = function () {
 
@@ -21,34 +36,49 @@ Lango.prototype.init = function () {
 }
 
 
-
 Lango.prototype.strToLang = function (str) {
-   // for (key in this.Language)if (str == key) return this.Language[key];
-   // throw Error = Error("Input Language Type Error")
+    // for (key in this.Language)if (str == key) return this.Language[key];
+    // throw Error = Error("Input Language Type Error")
 }
 
+Lango.prototype.setCountryCode = function (code) {
+    this.countryCode = code;
+}
+
+Lango.prototype.getGeoInfo = function () {
+
+    // TODO: DEBUG
+    $.getJSON("http://ip-api.com/json", function (result) {
+        this.setCountryCode(result.countryCode);
+    });
+
+}
 
 Lango.prototype.getLang = function () {
 
     var langFromCookie = $.cookie('lango_site_language');
     if (langFromCookie) return langFromCookie;
 
-    
-
 }
 
 
 Lango.prototype.translate = function (lang) {
 
-    var translating = []
-    $("span").each(function () {
-        if ($(this).attr("lang_id")) translating.push($(this))
-    });
+    if (this.isLangExist(lang)) {
 
-    translating.forEach(function (item) {
+        var translating = []
+        $("span").each(function () {
+            if ($(this).attr("lang_id")) translating.push($(this))
+        });
 
-        alert(item.attr("lang_id"))
+        translating.forEach(function (item) {
 
-    })
+            alert(item.attr("lang_id"))
+
+        })
+
+    }
+
+    else throw Error = Error("Input language is not in support list.")
 
 }
